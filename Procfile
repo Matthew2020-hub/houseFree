babba7723154b1d304632116104d58246e06964b
Matthew <akinolatolulope24@gmail.com>
2022-01-16 20:48:43 +0100

    connect heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dome.wsgi --log-file -
--- dome/settings.py
+++ dome/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0srq8vs)x)=#z5)wpj3h*mnwn&1%zk&#bci^_ftv#0io#sv5t7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['house-free.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
