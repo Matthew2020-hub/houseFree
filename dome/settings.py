f9d217bcfb6ef511dbdbce4914138395e9c77458
Matthew <akinolatolulope24@gmail.com>
2022-02-08 15:11:09 +0100

    debug

--- dome/settings.py
+++ dome/settings.py
@@ -100,7 +100,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['house-free.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','house-free.herokuapp.com']
 # ALLOWED_HOSTS = []
 # Application definition
 
