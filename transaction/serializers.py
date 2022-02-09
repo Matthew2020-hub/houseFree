4fc06357beb3e1e77a544870f07c9c8a66668995
Matthew <akinolatolulope24@gmail.com>
2022-02-09 14:05:26 +0100

    ee

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -4,21 +4,11 @@ from email.policy import default
 from django.forms import models
 from .models import Payment
 import uuid
-from rest_framework import serializers
-
-
-# class WalletSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         models = 
-#         fields = "__all__"
-        
+from rest_framework import serializers        
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
-
-    def __str__(self):
-        return self.amount
 
 class WithdrawalSerializer(serializers.Serializer):
     account_number = serializers.CharField()
